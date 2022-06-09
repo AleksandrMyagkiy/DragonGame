53aa67cc8ed9eba2cb9ae861cca21f9dab71a060
Aleksandr <dentranscom@gmail.com>
2022-06-09 10:25:15 +0300

    start: Dragon

---
+++ assets/scripts/main.js
@@ -0,0 +1,12 @@
+let config = {
+    type: Phaser.AUTO,
+    width: 1280,
+    height: 720,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    scene: [StartScene]
+};
+
+let game = new Phaser.Game(config);
